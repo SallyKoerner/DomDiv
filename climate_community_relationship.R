e29e251a7ec842539fc6da9ffee386fa4b17b7f9
klapierre <lapierre.kimberly@gmail.com>
2018-12-13 11:49:14 -0500

    Figures for plot-level and site-level community metrics.

--- climate_community_relationship.R
+++ climate_community_relationship.R
@@ -17,11 +17,16 @@ climate <- read.csv('ClimateSoilsData\\GPScorrClimate.csv')%>%
   left_join(key)%>%
   filter(!is.na(site))
 
-commClimate <- comm%>%
-  left_join(climate)
+
+###plot level
+commClimatePlot <- commPlot%>%
+  left_join(climate)%>%
+  group_by(block_trt, site)%>%
+  summarise_at(c('richness', 'Evar', 'bio1', 'bio12', 'datAI'), mean)%>%
+  ungroup()
 
 
-# #plot richness, Evar, EQ vs MAP
+# #plot richness, Evar vs MAP
 # ggplot(data=commClimate, aes(x=bio12, y=richness)) +
 #   geom_point() +
 #   facet_wrap(~block, scales='free')
@@ -29,10 +34,7 @@ commClimate <- comm%>%
 # ggplot(data=commClimate, aes(x=bio12, y=Evar)) +
 #   geom_point() +
 #   facet_wrap(~block, scales='free')
-# 
-# ggplot(data=commClimate, aes(x=bio12, y=EQ)) +
-#   geom_point() +
-#   facet_wrap(~block, scales='free')
+
 
 
 # #vs MAT
@@ -43,25 +45,34 @@ commClimate <- comm%>%
 # ggplot(data=commClimate, aes(x=bio1/10, y=Evar)) +
 #   geom_point() +
 #   facet_wrap(~block, scales='free')
-# 
-# ggplot(data=commClimate, aes(x=bio1/10, y=EQ)) +
-#   geom_point() +
-#   facet_wrap(~block, scales='free')
+
 
 
 #vs aridity
-ggplot(data=commClimate, aes(x=datAI, y=richness)) +
+ggplot(data=commClimatePlot, aes(x=datAI, y=richness)) +
   geom_point() +
   facet_wrap(~block_trt, scales='free_y') +
   xlab('Aridity') + ylab('Richness')
 
-ggplot(data=commClimate, aes(x=datAI, y=Evar)) +
+ggplot(data=commClimatePlot, aes(x=datAI, y=Evar)) +
   geom_point() +
   facet_wrap(~block_trt, scales='free') +
   xlab('Aridity') + ylab('Evar')
 
-ggplot(data=commClimate, aes(x=datAI, y=EQ)) +
+
+
+###site level
+commClimateSite <- commSite%>%
+  left_join(climate)
+
+#vs aridity
+ggplot(data=commClimateSite, aes(x=datAI, y=richness)) +
+  geom_point() +
+  facet_wrap(~block_trt, scales='free_y') +
+  xlab('Aridity') + ylab('Richness')
+
+ggplot(data=commClimateSite, aes(x=datAI, y=Evar)) +
   geom_point() +
   facet_wrap(~block_trt, scales='free') +
-  xlab('Aridity') + ylab('EQ')
+  xlab('Aridity') + ylab('Evar')
 
