39abfa683fe95a6db07558be3d27f89effb0f451
klapierre <lapierre.kimberly@gmail.com>
2018-12-13 14:47:30 -0500

    Remove MAP figures, because we've decided to use aridity only. Add models for each variable vs aridity.

--- climate_community_relationship.R
+++ climate_community_relationship.R
@@ -10,39 +10,43 @@ commSite <- read.csv('community_metrics_single_climate_Dec2018.csv')%>%
 
 
 #vs aridity
-ggplot(data=commClimateSite, aes(x=datAI, y=richness)) +
+ggplot(data=commSite, aes(x=datAI, y=richness)) +
   geom_point() +
   facet_wrap(~block_trt, scales='free_y') +
   xlab('Aridity') + ylab('Richness')
 
-ggplot(data=commClimateSite, aes(x=datAI, y=Evar)) +
+ggplot(data=commSite, aes(x=datAI, y=Evar)) +
   geom_point() +
   facet_wrap(~block_trt) +
   xlab('Aridity') + ylab('Evar')
 
-ggplot(data=commClimateSite, aes(x=datAI, y=ratio)) +
+ggplot(data=commSite, aes(x=datAI, y=ratio)) +
   geom_point() +
   facet_wrap(~block_trt) +
   xlab('Aridity') + ylab('Richness/Evar')
 
-#vs MAP
-ggplot(data=commClimateSite, aes(x=bio12, y=richness)) +
-  geom_point() +
-  facet_wrap(~block_trt, scales='free') +
-  xlab('MAP') + ylab('Richness')
-
-ggplot(data=commClimateSite, aes(x=bio12, y=Evar)) +
-  geom_point() +
-  facet_wrap(~block_trt, scales='free') +
-  xlab('MAP') + ylab('Evar')
-
-ggplot(data=commClimateSite, aes(x=bio12, y=ratio)) +
-  geom_point() +
-  facet_wrap(~block_trt) +
-  xlab('MAP') + ylab('Richness/Evar')
 
 #rich vs Evar
-ggplot(data=commClimateSite, aes(x=richness, y=Evar)) +
+ggplot(data=commSite, aes(x=richness, y=Evar)) +
   geom_point() +
   facet_wrap(~block_trt, scales='free') +
   xlab('richness') + ylab('Evar')
+
+
+#models for each variable
+richnessModels <- commSite%>%
+  group_by(block_trt)%>%
+  do(model = lm(richness ~ datAI, data = .))%>%
+  mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2])
+  
+evarModels <- commSite%>%
+  group_by(block_trt)%>%
+  do(model = lm(Evar ~ datAI, data = .))%>%
+  mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2])
+  
+ratioModels <- commSite%>%
+  group_by(block_trt)%>%
+  do(model = lm(ratio ~ datAI, data = .))%>%
+  mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2])
+
+  
