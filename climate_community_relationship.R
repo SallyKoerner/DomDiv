61e6fbf83529be8e47d2da3946a51df13c734396
klapierre <lapierre.kimberly@gmail.com>
2018-12-13 17:26:17 -0500

    Calculate geometric mean of aridity. Scale Evar and richness. Make new figures.

--- climate_community_relationship.R
+++ climate_community_relationship.R
@@ -6,7 +6,10 @@ library(tidyverse)
 
 #import community metrics - site level (single RAC for a site)
 commSite <- read.csv('community_metrics_single_climate_Dec2018.csv')%>%
-  mutate(ratio=richness/Evar)
+  mutate(ratio=richness/Evar)%>%
+  group_by(block_trt)%>%
+  mutate(Evar_scale=scale(Evar), richness_scale=scale(richness), AI_scale=scale(datAI))%>%
+  ungroup()
 
 
 # 
@@ -39,9 +42,14 @@ commSite <- read.csv('community_metrics_single_climate_Dec2018.csv')%>%
 
 
 #figure out climate mean, midpoint, min, max
+#function for geometric mean
+gm_mean = function(x, na.rm=TRUE){
+  exp(sum(log(x[x > 0]), na.rm=na.rm) / length(x))
+}
+
 climate <- commSite%>%
   group_by(block_trt)%>%
-  summarise(min_AI=min(datAI), max_AI=max(datAI), mean_AI=mean(datAI))%>%
+  summarise(min_AI=min(datAI), max_AI=max(datAI), mean_AI=mean(datAI), geo_mean_AI=gm_mean(datAI))%>%
   ungroup()%>%
   mutate(midpoint_AI=(min_AI+max_AI)/2)
 
@@ -50,42 +58,42 @@ climate <- commSite%>%
 #models for each variable
 richnessModels <- commSite%>%
   group_by(block_trt)%>%
-  do(model = lm(richness ~ datAI, data = .))%>%
+  do(model = lm(richness_scale ~ datAI, data = .))%>%
   mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2])%>%
   left_join(climate)%>%
   mutate(slope_sig=ifelse(pval>0.05, 0, slope))
 
 evarModels <- commSite%>%
   group_by(block_trt)%>%
-  do(model = lm(Evar ~ datAI, data = .))%>%
+  do(model = lm(Evar_scale ~ datAI, data = .))%>%
   mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2])%>%
   left_join(climate)
 
-ratioModels <- commSite%>%
-  group_by(block_trt)%>%
-  do(model = lm(ratio ~ datAI, data = .))%>%
-  mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2])%>%
-  left_join(climate)
+# ratioModels <- commSite%>%
+#   group_by(block_trt)%>%
+#   do(model = lm(ratio ~ datAI, data = .))%>%
+#   mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2])%>%
+#   left_join(climate)
 
 
 #model slopes vs aridity (comparing across blocks)
 #richness
-richnessAllFig <- ggplot(data=commSite, aes(x=datAI, y=richness, color=block_trt)) +
+richnessAllFig <- ggplot(data=commSite, aes(x=datAI, y=richness_scale, color=block_trt)) +
   geom_point() +
-  xlab('Aridity') + ylab('Richness') +
+  xlab('Aridity') + ylab('Scaled Richness') +
   geom_smooth(method='lm') +
   theme(legend.position='none')
 
-summary(lm(slope~midpoint_AI, data=richnessModels))
+summary(lm(slope~geo_mean_AI, data=richnessModels))
 
-richnessSlopeFig <- ggplot(data=richnessModels, aes(x=midpoint_AI, y=slope, color=block_trt)) +
+richnessSlopeFig <- ggplot(data=richnessModels, aes(x=geo_mean_AI, y=slope, color=block_trt)) +
   geom_point(size=5) +
   geom_errorbarh(aes(xmin=min_AI, xmax=max_AI)) +
   geom_errorbar(aes(ymin=slope-slope_err, ymax=slope+slope_err)) +
   xlab('Aridity') + ylab('Slope of Richness v Aridity') +
   geom_hline(yintercept=0) +
   geom_smooth(method='lm', size=2, color='black') +
-  annotate("text", x=1.4, y=125, label = "R2=0.297,\np=0.094", size=8)
+  annotate("text", x=1.4, y=6, label = "R2=0.526,\np=0.042", size=8)
 
 #richness figure
 pushViewport(viewport(layout=grid.layout(1,2)))
@@ -95,22 +103,22 @@ print(richnessSlopeFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
 
 
 #Evar
-evennessAllFig <- ggplot(data=commSite, aes(x=datAI, y=Evar, color=block_trt)) +
+evennessAllFig <- ggplot(data=commSite, aes(x=datAI, y=Evar_scale, color=block_trt)) +
   geom_point() +
-  xlab('Aridity') + ylab('Evar') +
+  xlab('Aridity') + ylab('Scaled Evar') +
   geom_smooth(method='lm') +
   theme(legend.position='none')
 
-summary(lm(slope~midpoint_AI, data=evarModels))
+summary(lm(slope~geo_mean_AI, data=evarModels))
 
-evennessSlopeFig <- ggplot(data=evarModels, aes(x=midpoint_AI, y=slope, color=block_trt)) +
+evennessSlopeFig <- ggplot(data=evarModels, aes(x=geo_mean_AI, y=slope, color=block_trt)) +
   geom_point(size=5) +
   geom_errorbarh(aes(xmin=min_AI, xmax=max_AI)) +
   geom_errorbar(aes(ymin=slope-slope_err, ymax=slope+slope_err)) +
   xlab('Aridity') + ylab('Slope of Evar v Aridity') +
   geom_hline(yintercept=0) +
   # geom_smooth(method='lm', size=2, color='black') +
-  annotate("text", x=1.4, y=-0.6, label = "R2=0.072,\np=0.520", size=8)
+  annotate("text", x=4, y=-0.6, label = "R2=0.022,\np=0.725", size=8)
 
 #Evar figure
 pushViewport(viewport(layout=grid.layout(1,2)))
@@ -119,25 +127,56 @@ print(evennessSlopeFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
 
 
 
-#Ratio richness/Evar
-ratioAllFig <- ggplot(data=commSite, aes(x=datAI, y=ratio, color=block_trt)) +
+# #Ratio richness/Evar
+# ratioAllFig <- ggplot(data=commSite, aes(x=datAI, y=ratio, color=block_trt)) +
+#   geom_point() +
+#   xlab('Aridity') + ylab('Richness/Evar') +
+#   geom_smooth(method='lm') +
+#   theme(legend.position='none')
+# 
+# summary(lm(slope~midpoint_AI, data=ratioModels))
+# 
+# ratioSlopeFig <- ggplot(data=ratioModels, aes(x=midpoint_AI, y=slope, color=block_trt)) +
+#   geom_point(size=5) +
+#   geom_errorbarh(aes(xmin=min_AI, xmax=max_AI)) +
+#   geom_errorbar(aes(ymin=slope-slope_err, ymax=slope+slope_err)) +
+#   xlab('Aridity') + ylab('Slope of Richness/Evar v Aridity') +
+#   geom_hline(yintercept=0) +
+#   geom_smooth(method='lm', size=2, color='black') +
+#   annotate("text", x=1.4, y=400, label = "R2=0.526,\np=0.042", size=8)
+# 
+# #ratio figure
+# pushViewport(viewport(layout=grid.layout(1,2)))
+# print(ratioAllFig, vp=viewport(layout.pos.row=1, layout.pos.col=1))
+# print(ratioSlopeFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
+
+
+
+
+
+#Evar vs lots of things
+ggplot(data=commSite, aes(x=bio12, y=Evar, color=block_trt)) +
   geom_point() +
-  xlab('Aridity') + ylab('Richness/Evar') +
   geom_smooth(method='lm') +
-  theme(legend.position='none')
+  xlab('MAP')
 
-summary(lm(slope~midpoint_AI, data=ratioModels))
+ggplot(data=commSite, aes(x=bio1/10, y=Evar, color=block_trt)) +
+  geom_point() +
+  geom_smooth(method='lm') +
+  xlab('MAT')
 
-ratioSlopeFig <- ggplot(data=ratioModels, aes(x=midpoint_AI, y=slope, color=block_trt)) +
-  geom_point(size=5) +
-  geom_errorbarh(aes(xmin=min_AI, xmax=max_AI)) +
-  geom_errorbar(aes(ymin=slope-slope_err, ymax=slope+slope_err)) +
-  xlab('Aridity') + ylab('Slope of Richness/Evar v Aridity') +
-  geom_hline(yintercept=0) +
-  geom_smooth(method='lm', size=2, color='black') +
-  annotate("text", x=1.4, y=400, label = "R2=0.526,\np=0.042", size=8)
+ggplot(data=commSite, aes(x=altitude, y=Evar, color=block_trt)) +
+  geom_point() +
+  geom_smooth(method='lm') +
+  xlab('Elevation')
 
-#ratio figure
-pushViewport(viewport(layout=grid.layout(1,2)))
-print(ratioAllFig, vp=viewport(layout.pos.row=1, layout.pos.col=1))
-print(ratioSlopeFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
+
+
+
+
+
+
+
+
+
+
