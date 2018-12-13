38ebafb10d49b915924e6b36bc8810806dc1a838
klapierre <lapierre.kimberly@gmail.com>
2018-12-13 11:42:41 -0500

    Took code from here to subset plots out of sites, and put into the RichEvenMetrics.R script.

--- climate_community_relationship.R
+++ climate_community_relationship.R
@@ -2,8 +2,11 @@ setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\DomDiv_Workshop\
 
 library(tidyverse)
 
-#import community metrics
-comm <- read.csv('community_metrics_12122018.csv')
+#import community metrics - plot level (each plot represented)
+commPlot <- read.csv('community_metrics_plot_Dec2018.csv')
+
+#import community metrics - site level (single RAC for a site)
+commSite <- read.csv('community_metrics_single_Dec2018.csv')
 
 #import key to merge community metrics and climate data
 key <- read.csv('ClimateSoilsData\\SiteNameKey.csv')
@@ -15,34 +18,8 @@ climate <- read.csv('ClimateSoilsData\\GPScorrClimate.csv')%>%
   filter(!is.na(site))
 
 commClimate <- comm%>%
-  left_join(climate)%>%
-  mutate(plot_size=quadrat.width*quadrat.length)%>%
-  #create block_trt column to account for grazing and plot size differences for a few sites
-  mutate(block_trt=as.factor(ifelse(block=='Tibet'&trt=='G', 'Tibet_grazed', ifelse(block=='Tibet'&trt=='U', 'Tibet_ungrazed', ifelse(block=='SAmerica'&trt=='G', 'SAmerica_grazed', ifelse(block=='SAmerica'&trt=='U', 'SAmerica_ungrazed', ifelse(block=='Canada'&trt=='in', 'Canada_ungrazed', ifelse(block=='Canada'&trt=='out', 'Canada_grazed', ifelse(block=='AUS'&plot_size==100, 'AUS_100_m2', ifelse(block=='AUS'&plot_size==900, 'AUS_900_m2', ifelse(block=='AUS'&plot_size==2500, 'AUS_2500_m2', as.character(block))))))))))))%>%
-  filter(block_trt!='AUS') #drops Australian plots that are mis-sized (very uncommon compared to standard sizes)
+  left_join(climate)
 
-china <- commClimate%>%
-  filter(block_trt=='China')%>%
-  filter(site!='GCN-Dangxiong')%>% #this site has only 12 reps and is similar in aridity to another site
-  mutate(random_num=sample(1:1000, 531, replace=F))%>%
-  group_by(site)%>%
-  mutate(rank=order(order(random_num)))%>% #create rank order of the random numbers
-  ungroup()%>%
-  filter(rank<40)%>% #filter to only keep 39 plots per site
-  select(-random_num, -rank)
-
-brazil <- commClimate%>%
-  filter(block_trt=='Brazil')%>%
-  separate(Site_num, into=c('region', 'CCA', 'rep'), sep='_')%>%
-  filter(rep=='PT01')%>% #subsetting out one group from each region to make a site so that there are not tons of sites per region (13 total sites now matching with 13 total region)
-  rename(Site_num=rep)%>%
-  select(-region, -CCA)
-  
-commClimateSubset <- commClimate%>%
-  filter(block_trt!='China'&block_trt!='Brazil')%>%
-  rbind(china)%>%
-  rbind(brazil)%>%
-  filter(block!='Canada')
 
 # #plot richness, Evar, EQ vs MAP
 # ggplot(data=commClimate, aes(x=bio12, y=richness)) +
