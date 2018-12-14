0949658da24090b89dc8b3ca86aee59cb41abc57
klapierre <lapierre.kimberly@gmail.com>
2018-12-14 10:38:25 -0500

    Adding figure for richness vs evar, making scaled figures, exporting tables

--- climate_community_relationship.R
+++ climate_community_relationship.R
@@ -1,7 +1,20 @@
 setwd('C:\\Users\\lapie\\Dropbox (Smithsonian)\\working groups\\DomDiv_Workshop\\Dominance_Diversity')
 
 library(grid)
+library(knitr)
+library(kableExtra)
 library(tidyverse)
+
+
+###ggplot theme set
+theme_set(theme_bw())
+theme_update(axis.title.x=element_text(size=20, vjust=-0.35), axis.text.x=element_text(size=16),
+             axis.title.y=element_text(size=20, angle=90, vjust=0.5), axis.text.y=element_text(size=16),
+             plot.title = element_text(size=24, vjust=2),
+             panel.grid.major=element_blank(), panel.grid.minor=element_blank(),
+             legend.title=element_blank(), legend.text=element_text(size=20))
+
+
 
 
 #import community metrics - site level (single RAC for a site)
@@ -59,15 +72,39 @@ climate <- commSite%>%
 richnessModels <- commSite%>%
   group_by(block_trt)%>%
   do(model = lm(richness_scale ~ datAI, data = .))%>%
-  mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2])%>%
+  mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2], f=summary(model)$fstatistic[1], df_num=summary(model)$fstatistic[2], df_den=summary(model)$fstatistic[3])%>%
   left_join(climate)%>%
   mutate(slope_sig=ifelse(pval>0.05, 0, slope))
+
+richnessModelTable <- richnessModels%>%
+  select(block_trt, f, df_num, df_den, pval, R2, slope)%>%
+  rename(Block=block_trt)
+kable(richnessModelTable, 'html')%>%
+  cat(., file = "richnessModelTable.html")
 
 evarModels <- commSite%>%
   group_by(block_trt)%>%
   do(model = lm(Evar_scale ~ datAI, data = .))%>%
-  mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2])%>%
+  mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2], f=summary(model)$fstatistic[1], df_num=summary(model)$fstatistic[2], df_den=summary(model)$fstatistic[3])%>%
   left_join(climate)
+
+evarModelTable <- evarModels%>%
+  select(block_trt, f, df_num, df_den, pval, R2, slope)%>%
+  rename(Block=block_trt)
+kable(evarModelTable, 'html')%>%
+  cat(., file = "evarModelTable.html")
+
+compareModels <- commSite%>%
+  group_by(block_trt)%>%
+  do(model = lm(Evar_scale ~ richness_scale, data = .))%>%
+  mutate(R2=summary(model)$r.squared, pval=summary(model)$coefficients[2,4], slope=summary(model)$coefficients[2], slope_err=summary(model)$coefficients[2,2], f=summary(model)$fstatistic[1], df_num=summary(model)$fstatistic[2], df_den=summary(model)$fstatistic[3])%>%
+  left_join(climate)
+
+compareModelTable <- compareModels%>%
+  select(block_trt, f, df_num, df_den, pval, R2, slope)%>%
+  rename(Block=block_trt)
+kable(compareModelTable, 'html')%>%
+  cat(., file = "compareModelTable.html")
 
 # ratioModels <- commSite%>%
 #   group_by(block_trt)%>%
@@ -99,7 +136,7 @@ richnessSlopeFig <- ggplot(data=richnessModels, aes(x=geo_mean_AI, y=slope, colo
 pushViewport(viewport(layout=grid.layout(1,2)))
 print(richnessAllFig, vp=viewport(layout.pos.row=1, layout.pos.col=1))
 print(richnessSlopeFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
-
+#export at 1800x800
 
 
 #Evar
@@ -118,12 +155,40 @@ evennessSlopeFig <- ggplot(data=evarModels, aes(x=geo_mean_AI, y=slope, color=bl
   xlab('Aridity') + ylab('Slope of Evar v Aridity') +
   geom_hline(yintercept=0) +
   # geom_smooth(method='lm', size=2, color='black') +
-  annotate("text", x=4, y=-0.6, label = "R2=0.022,\np=0.725", size=8)
+  annotate("text", x=1.2, y=4, label = "R2=0.022,\np=0.725", size=8)
 
 #Evar figure
 pushViewport(viewport(layout=grid.layout(1,2)))
 print(evennessAllFig, vp=viewport(layout.pos.row=1, layout.pos.col=1))
 print(evennessSlopeFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
+#export at 1800x800
+
+
+
+#Evar v richness
+compareAllFig <- ggplot(data=commSite, aes(x=richness_scale, y=Evar_scale, color=block_trt)) +
+  geom_point() +
+  xlab('Scaled Richness') + ylab('Scaled Evar') +
+  geom_smooth(method='lm') +
+  theme(legend.position='none')
+
+summary(lm(slope~geo_mean_AI, data=compareModels))
+summary(lm(slope~geo_mean_AI, data=subset(compareModels, block_trt!='China')))
+
+compareSlopeFig <- ggplot(data=compareModels, aes(x=geo_mean_AI, y=slope, color=block_trt)) +
+  geom_point(size=5) +
+  geom_errorbarh(aes(xmin=min_AI, xmax=max_AI)) +
+  geom_errorbar(aes(ymin=slope-slope_err, ymax=slope+slope_err)) +
+  xlab('Aridity') + ylab('Slope of Evar v Richness') +
+  geom_hline(yintercept=0) +
+  geom_smooth(data=subset(compareModels, block_trt!='China'), method='lm', size=2, color='black') +
+  annotate("text", x=1.2, y=-0.5, label = "R2=0.292,\np=0.166\nR2=0.798,\np=0.007", size=8)
+
+#comparison figure
+pushViewport(viewport(layout=grid.layout(1,2)))
+print(compareAllFig, vp=viewport(layout.pos.row=1, layout.pos.col=1))
+print(compareSlopeFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
+#export at 1800x800
 
 
 
